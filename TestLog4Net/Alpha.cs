5701ace5712f154115b2f6f82c1b1f3fddd24796
sfjpk31 <otjpk31@gmail.com>
2013-11-07 12:56:55 -0800

    Added TestLog4Net project

---
+++ TestLog4Net/Alpha.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using OpenTable.Umami.Common.Logging;
+
+namespace TestLog4Net
+{
+    public class Alpha
+    {
+        protected static readonly ILogger Log = new Log4NetLogger("TestLog4Net.Alpha");
+
+        private const string Publisher = "TestLog4Net";
+        private const string Category = "Alpha";                                     
+
+        public Alpha(int index)
+        {
+            Index = index;
+        }
+
+        public int Index { get; set; }
+
+        public void DoAlphaOne()
+        {
+            Log.LogDebug("In DoAlphaOne Index = " + Index.ToString(CultureInfo.InvariantCulture));
+            Log.LogDebug(Publisher, Category, "In DoAlphaOne Index = " + Index.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public void DoAlphaTwo()
+        {
+            Log.LogDebug("In DoAlphaTwo Index = " + Index.ToString(CultureInfo.InvariantCulture));
+            Log.LogDebug(Publisher, Category, "In DoAlphaTwo Index = " + Index.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ TestLog4Net/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Threading;
+using OpenTable.Umami.Common.Logging;
+using System;
+
+namespace TestLog4Net
+{
+    class Program
+    {
+
+        protected static readonly bool UseThreading = true;
+
+        static void Main()
+        {
+            ILogger log = new Log4NetLogger("TestLog4Net.Program");
+
+            const string publisher = "TestLog4Net";
+            const string category = "Main";
+
+            log.LogDebug("This is a Debug log message");
+            log.LogDebug(publisher, category, "This is a Debug log message with publisher and category");
+            log.LogInfo("This is an info log message");
+            log.LogInfo(publisher, category, "This is an info log message with publisher and category");
+            log.LogWarning("This is a Warning log message");
+            log.LogWarning(publisher, category, "This is a Warning log message with publisher and category");
+            log.LogError("This is an Error log message");
+            log.LogError(publisher, category, "This is an Error log message with publisher and category");
+            log.LogFatal("This is a Fatal log message");
+            log.LogFatal(publisher, category, "This is a Fatal log message with publisher and category");
+
+            for (var i = 1; i <= 10; ++i )
+            {
+                if (UseThreading)
+                {
+                    var t = new Thread(ProcessAlpha);
+                    log.LogDebug("Created thread " + t.ManagedThreadId.ToString(CultureInfo.InvariantCulture));
+                    t.Start(i);
+                }
+                else
+                {
+                    ProcessAlpha(i);
+                }
+            }
+            
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        }
+
+        static void ProcessAlpha(object index)
+        {
+            var a = new Alpha((int) index);
+            a.DoAlphaOne();
+            a.DoAlphaTwo();
+        }
+    }
+}
